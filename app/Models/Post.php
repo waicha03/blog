50f65e91b7ce640e548c4c47a7f5c29935b544b2
waicha03 <21ib050b@rikkyo.ac.jp>
2024-06-03 18:37:23 +0900

    8-4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
+{   protected $fillable = [
+    'title',
+    'body',
+    ];
+    
     use HasFactory;
     
     public function getByLimit(int $limit_count = 10)
